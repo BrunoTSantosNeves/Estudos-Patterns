d0d8bcef382c69b8af7be10e164ac8ef31538003
bidbibb <tonetti583@gmail.com>
2025-03-11 20:35:55 -0300

    feat: Adapter converter XML to json

--- Adapter/src/Main.java
+++ Adapter/src/Main.java
@@ -25,10 +25,62 @@ class Adapter implements NewSystem {
     }
 }
 
-public class Main {
+/*
+ * public class Main {
     public static void main(String[] args) {
         OldSystem oldSystem = new OldSystem();
         NewSystem newSystem = new Adapter(oldSystem);
         System.out.println(newSystem.getdata());
+    }
+}
+ */
+
+ 
+
+// Version: 2.1
+// Adapter Pattern 
+
+// Classe Legada (Retorna XML)
+class SistemaAntigo {
+    public String getXMLData() {
+        return "<data>Old System Data</data>";
+    }
+}
+
+// Interface esperada pelo novo sistema (Retorna JSON)
+interface JsonDataProvider {
+    Object getData();
+}
+
+// Conversor de XML para JSON
+class ToJsonConverter {
+    public static String convert(String xmlData) {
+        return "{\"name\": \"Old System Data (converted)\" }";
+    }
+}
+
+// Adapter
+class Adaptador implements JsonDataProvider {
+    private final SistemaAntigo sistemaAntigo;
+    
+    public Adaptador(SistemaAntigo sistemaAntigo) {
+        this.sistemaAntigo = sistemaAntigo;
+    }
+
+    @Override
+    public Object getData() {
+        String xmlData = sistemaAntigo.getXMLData();
+        return ToJsonConverter.convert(xmlData);
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        SistemaAntigo sistemaAntigo = new SistemaAntigo();
+        JsonDataProvider adapter = new Adaptador(sistemaAntigo);
+    
+        System.out.println(adapter.getData());
+       
     }
 }
