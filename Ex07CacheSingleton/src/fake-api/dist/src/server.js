0d1db02e5cd2e3ec018aa9e5ccbf574eed80d863
bidbibb <tonetti583@gmail.com>
2025-03-27 16:30:06 -0300

    feat: Created directory dist and server is run in localhost

--- Ex07CacheSingleton/src/fake-api/dist/src/server.js
+++ Ex07CacheSingleton/src/fake-api/dist/src/server.js
@@ -1,43 +1,40 @@
-import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import JsonDatabase from '../infrascructure/JsonDatabase';
-
-const app: Application = express();
-const db = new JsonDatabase();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const JsonDatabase_1 = __importDefault(require("../infrascructure/JsonDatabase"));
+const app = (0, express_1.default)();
+const db = new JsonDatabase_1.default();
 const PORT = 3001;
-
-app.use(bodyParser.json());
-
+app.use(body_parser_1.default.json());
 // Buscar todos os usuários
-app.get('/users', (req: Request, res: Response): void => {
+app.get('/users', (req, res) => {
     const users = db.getAll('users');
     res.json(users);
 });
-
 // Buscar um usuário por ID
-app.get('/users/:id', (req: Request, res: Response): void => {
+app.get('/users/:id', (req, res) => {
     const userId = req.params.id;
     const user = db.getById('users', userId);
-
     if (!user) {
         res.status(404).json({ error: 'Usuário não encontrado' });
         return;
     }
     res.json(user);
 });
-
 // Adicionar um novo usuário
-app.post('/users', (req: Request, res: Response): void => {
+app.post('/users', (req, res) => {
     const newUser = req.body;
     db.insert('users', newUser);
     res.status(201).json({ message: 'Usuário criado com sucesso!' });
 });
-
 // Atualizar um usuário por ID
-app.put('/users/:id', (req: Request, res: Response): void => {
+app.put('/users/:id', (req, res) => {
     const userId = req.params.id;
     const updatedUser = req.body;
-
     const success = db.update('users', userId, updatedUser);
     if (!success) {
         res.status(404).json({ error: 'Usuário não encontrado' });
@@ -45,19 +42,16 @@ app.put('/users/:id', (req: Request, res: Response): void => {
     }
     res.json({ message: 'Usuário atualizado com sucesso!' });
 });
-
 // Remover um usuário por ID
-app.delete('/users/:id', (req: Request, res: Response): void => {
+app.delete('/users/:id', (req, res) => {
     const userId = req.params.id;
     const success = db.remove('users', userId);
-    
     if (!success) {
         res.status(404).json({ error: 'Usuário não encontrado' });
         return;
     }
     res.json({ message: 'Usuário removido com sucesso!' });
 });
-
 // Iniciar o servidor
 app.listen(PORT, () => {
     console.log(` Servidor rodando em http://localhost:${PORT}`);
